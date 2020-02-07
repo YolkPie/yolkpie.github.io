1c4fab144e54fc8bf1c4acd6fcbdf144a416ff07
yujihu <yujihu@jd.com>
2020-02-07 10:33:06 +0000

    travis-ci auto build yolk-cli

---
+++ yolk-cli/assets/js/15.434572f2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{219:function(t,e,s){"use strict";s.r(e);var n=s(0),r=Object(n.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("h1",{attrs:{id:"react-单页应用模板"}},[this._v("react 单页应用模板")])])}),[],!1,null,null,null);e.default=r.exports}}]);
