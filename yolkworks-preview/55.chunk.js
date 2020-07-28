42c46edacbd4e346f33afe224b1e4c0ebd04b56a
yolk-pie <yujihu@jd.com>
2020-07-28 09:07:53 +0000

    travis-ci auto build yolkworks-preview

--- yolkworks-preview/55.chunk.js
+++ yolkworks-preview/55.chunk.js
@@ -1,3 +1,3 @@
-webpackJsonp([55],{126:/*!***********************************************************************************************!*\
+webpackJsonp([55],{122:/*!***********************************************************************************************!*\
   !*** ./node_modules/raw-loader!./src/boilerplates/custom/vue-blocks-BasicNotFound/codepan.js ***!
   \***********************************************************************************************//*! dynamic exports provided *//*! all exports used */function(a){a.exports="new Vue({\n    name: 'BasicNotFound'\n}\n).$mount('#app')\n"}});
--- yolkworks-preview/61.chunk.js
+++ yolkworks-preview/61.chunk.js
@@ -1,3 +1,3 @@
-webpackJsonp([61],{120:/*!*********************************************************************************************!*\
-  !*** ./node_modules/raw-loader!./src/boilerplates/custom/react-blocks-EntryCard/codepan.js ***!
-  \*********************************************************************************************//*! dynamic exports provided *//*! all exports used */function(a){a.exports="new Vue({}).$mount('#app')\n"}});
+webpackJsonp([61],{116:/*!*************************************************************************************************!*\
+  !*** ./node_modules/raw-loader!./src/boilerplates/custom/react-blocks-BasicNotFound/codepan.js ***!
+  \*************************************************************************************************//*! dynamic exports provided *//*! all exports used */function(a){a.exports="new Vue({}).$mount('#app')\n"}});
